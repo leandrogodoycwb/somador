815e98ff9712445ab7adbee685f83c4d5f0a38a3
leandrogodoycwb <53630327+leandrogodoycwb@users.noreply.github.com>
2020-08-20 17:58:37 -0300

    trabalho do dia 20/08
    Somador java

---
+++ Aula03/src/com/github/leandrogodoycwb/prog2/aula03/somador/Somador.java
@@ -0,0 +1,15 @@
+package com.github.leandrogodoycwb.prog2.aula03.somador;
+
+public class Somador {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++)
+			System.out.println(args[i]);
+		 
+	}
+	
+	 int total = 0;
+	 
+	
+	
+}
